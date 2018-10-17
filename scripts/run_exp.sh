185e0ff769618bde68d54e87b67b05dea2f088d5
milkschen <s93rm6@gmail.com>
2018-10-17 00:30:01 -0400

    ht2: use no-spliced

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -15,9 +15,9 @@ ht_ref_9=$dir/chr9/chr9
 ht_sam=$dir/alignments/na12878-hapA-wg-ht2.sam
 ht_sam_9=$dir/alignments/na12878-hapA-chr9-ht2.sam
 
-ht_g_ref=/scratch/groups/blangme2/jacob/vis_exp_chr9_NA19238/hisat_indexes/popcov10
+ht_g_ref=/scratch/groups/blangme2/jacob/vis_exp_chr9_NA19238/hisat_indexes/popcov70
 #ht_g_sam=$dir/alignments/na12878-chr9-phase3_popcov10_hapA-1M-ht2.sam
-ht_g_sam=$dir/alignments/na12878-chr9-phase3_popcov10_hapA-1M-ht2.sam
+ht_g_sam=$dir/alignments/na12878-chr9-phase3_popcov70_hapA-1M-ht2.sam
 
 bt_per_ref=$dir/na12878/indexes/na12878-chr9-phase3_hapA
 bt_per_sam=$dir/alignments/na12878-chr9-phase3_hapA-1M-bt2-per.sam
@@ -43,9 +43,9 @@ ht_per_sam=$dir/alignments/na12878-hapA-chr9-per-ht2.sam
 #echo "bt2, personalized chr9"
 #time bowtie2 -p 4 -x $bt_per_ref -U $fq -S $bt_per_sam 
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $bt_per_sam
-#echo "ht2, C9_PG"
-#time hisat2 -p 4 -x $ht_per_ref -U $fq -S $ht_per_sam
-#python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_per_sam
+echo "ht2, C9_PG"
+time hisat2 -p 4 -x $ht_per_ref -U $fq -S $ht_per_sam --no-spliced-alignment
+python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_per_sam
 
 #echo "bt2, whole genome"
 #time bowtie2 -p 4 -x $bt_ref -U $fq -S $bt_sam 
@@ -58,27 +58,26 @@ bwa_per_sam=$dir/alignments/na12878-hapA-chr9-bwamem-per.sam
 bwa_sam_9=$dir/alignments/na12878-hapA-chr9-bwamem.sam
 bwa_sam=$dir/alignments/na12878-hapA-wg-bwamem.sam
 bwa_per_ref=$dir/na12878/na12878-chr9-phase3_hapA.fa
-#bwa_ref=$ARRAY/indexes/hg19.fa
+bwa_ref=$ARRAY/indexes/hs37d5.fa
 bwa_ref_9=$dir/chr9/chr9.fa
 
 #time bwa mem -t 4 $bwa_per_ref $fq > $bwa_per_sam
 #time bwa mem -t 4 $bwa_ref $fq > $bwa_sam
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $bwa_sam
-time bwa mem -t 4 $bwa_ref_9 $fq > $bwa_sam_9
-python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $bwa_sam_9
+#time bwa mem -t 4 $bwa_ref_9 $fq > $bwa_sam_9
+#python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $bwa_sam_9
 
 #echo "ht2, whole genome (linear genome)"
-#time hisat2 -p 4 -x $ht_ref -U $fq -S $ht_sam
+#time hisat2 -p 4 -x $ht_ref -U $fq -S $ht_sam --no-spliced-alignment
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_sam
 #echo "ht2, chr 9"
-#time hisat2 -p 4 -x $ht_ref_9 -U $fq -S $ht_sam_9
+#time hisat2 -p 4 -x $ht_ref_9 -U $fq -S $ht_sam_9 --no-spliced-alignment
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_sam_9
-
 #echo "ht2, whole genome with snps (provided by ht2)"
-#time hisat2 -p 4 -x $ht_ref_snp -U $fq -S $ht_sam
+#time hisat2 -p 4 -x $ht_ref_snp -U $fq -S $ht_sam --no-spliced-alignment
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_sam
 
 # Graph
-#echo "ht2, popcov chr9 genome"
-#time hisat2 -p 4 -x $ht_g_ref -U $fq -S $ht_g_sam
+#echo "ht2, popcov chr9 genome: 70"
+#time hisat2 -p 4 -x $ht_g_ref -U $fq -S $ht_g_sam --no-spliced-alignment
 #python -O $dir/scripts/analyze_sam.py -s 1 -g $g_sam -n $ht_g_sam
