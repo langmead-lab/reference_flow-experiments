c3060f5db56ce60f6a48e21c9f675b4540f5a647
milkschen <s93rm6@gmail.com>
2019-01-23 16:36:51 -0500

    untrack some scripts

--- scripts/gen_hap.sh
+++
@@ -1,7 +0,0 @@
-python /home-1/cnaechy1@jhu.edu/scratch/FORGe-experiments/scripts/update_genome.py \
-    --ref $ARRAY/indexes/hs37d5.fa \
-    --vcf ALL.chr9.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf \
-    --chrom 9 \
-    --out-prefix na12878-chr9-phase3 \
-    --name NA12878
-    #--vcf /home-1/cnaechy1@jhu.edu/scratch/relaxing/na12878/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf \
--- scripts/simulate_reads.sh
+++
@@ -1,4 +0,0 @@
-/scratch/groups/blangme2/naechyun/software/mason-0.1.2-Linux-x86_64/bin/mason \
-    illumina /home-1/cnaechy1@jhu.edu/scratch/relaxing/na12878/na12878-chr9-phase3_hapA.fa \
-    -N 1000000 -sq -n 100 -hs 0 -hi 0 -hn 1 -mp \
-    -o na12878-chr9-phase3_hapA-1M.fq
