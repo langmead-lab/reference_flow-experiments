72fec5e9e0af50dec23eeffe4fa5682527bea63b
milkschen <nchen18@illinois.edu>
2019-01-26 18:04:54 -0500

    shows mapq summary stats

--- scripts/compare_sam.py
+++ scripts/compare_sam.py
@@ -46,11 +46,11 @@ def parse_args():
     READ_LEN = args.read_len
     return args
 
-def check_var(dict, names, ref_index, main_index, alt_index, per):
+def check_var(dict_var, names, ref_index, main_index, alt_index, per):
     list_names = list(names)
     list_num_var = []
     for ll in list_names:
-        var = dict[ll]
+        var = dict_var[ll]
         if per:
             num_vars = check_var_in_region(var, main_index=main_index, alt_index=alt_index, MAIN_CHRM=MAIN_CHRM, ALT_CHRM=ALT_CHRM, READ_LEN=READ_LEN)
         elif per == 0:
@@ -66,6 +66,23 @@ def check_var(dict, names, ref_index, main_index, alt_index, per):
     print ('  Max num of vars =', max_num_var)
     print ()
 
+def check_aln(target, dict_ref, dict_per, item='mapq'):
+    list_target = list(target)
+    target_on_ref = []
+    target_on_per = []
+    if dict_ref != None:
+        if 'mapq' in item:
+            for i in list_target:
+                target_on_ref.append(dict_ref[i].mapq)
+            avg_mapq = float(sum(target_on_ref)) / len(target_on_ref)
+            print ('  Avg mapq on ref', avg_mapq)
+    if dict_per != None:
+        if 'mapq' in item:
+            for i in list_target:
+                target_on_per.append(dict_per[i].mapq)
+            avg_mapq = float(sum(target_on_per)) / len(target_on_per)
+            print ('  Avg mapq on per', avg_mapq)
+    
 def compare_sam(args):
     ref_c_fn = args.ref_correct
     ref_ic_fn = args.ref_incorrect
@@ -78,10 +95,11 @@ def compare_sam(args):
     per_c_f = open(per_c_fn, 'r')
     per_ic_f = open(per_ic_fn, 'r')
     
-    var_list = read_var(var_fn, remove_conflict=True, remove_coexist=True)
+    var_list = read_var(var_fn, remove_conflict=True, remove_coexist=False)
     main_index, alt_index = build_index(var_list, per=2, MAIN_STRAND=MAIN_STRAND, ALT_STRAND=ALT_STRAND)
     ref_index, _ = build_index(var_list, per=0, MAIN_STRAND=MAIN_STRAND, ALT_STRAND=ALT_STRAND)
 
+    # Reads aligned to personalized genomes
     dict_per_c = {}
     dict_per_ic = {}
     dict_per_u = {}
@@ -97,6 +115,7 @@ def compare_sam(args):
             dict_per_u[name] = info
         else:
             dict_per_ic[name] = info
+    # Reads aligned to standard ref genome
     dict_ref_c = {}
     dict_ref_ic = {}
     dict_ref_u = {}
@@ -129,42 +148,58 @@ def compare_sam(args):
     num_total = len(dict_pc_rc) + len(dict_pc_ric) + len(dict_pc_ru) + len(dict_pu_rc) + len(dict_pu_ru) + len(dict_pu_ric) + len(dict_pic_rc) + len(dict_pic_ru) + len(dict_pic_ric)
 
     print ('Num of ref-c and per-c =', len(dict_pc_rc))
+    print ('  Looking dict_pc_rc...')
+    check_aln(target=dict_pc_rc, dict_ref=dict_ref_c, dict_per=dict_per_c, item='mapq')
     print ('  Looking dict_pc_rc in ref...')
     check_var(dict_ref_c, dict_pc_rc, ref_index, main_index, alt_index, per=0)
     print ('  Looking dict_pc_rc in per...')
     check_var(dict_per_c, dict_pc_rc, ref_index, main_index, alt_index, per=2)
 
     print ('Num of ref-c and per-ic =', len(dict_pic_rc))
+    print ('  Looking dict_pic_rc...')
+    check_aln(target=dict_pic_rc, dict_ref=dict_ref_c, dict_per=dict_per_ic, item='mapq')
     print ('  Looking dict_pic_rc in ref...')
     check_var(dict_ref_c, dict_pic_rc, ref_index, main_index, alt_index, per=0)
     print ('  Looking dict_pic_rc in per...')
     check_var(dict_per_ic, dict_pic_rc, ref_index, main_index, alt_index, per=2)
     
     print ('Num of ref-c and per-u =', len(dict_pu_rc))
+    print ('  Looking dict_pu_rc...')
+    check_aln(target=dict_pu_rc, dict_ref=dict_ref_c, dict_per=None, item='mapq')
     print ('  Looking dict_pu_rc in ref...')
     check_var(dict_ref_c, dict_pu_rc, ref_index, main_index, alt_index, per=0)
 
     print ('Num of ref-ic and per-c =', len(dict_pc_ric))
+    print ('  Looking dict_pc_ric...')
+    check_aln(target=dict_pc_ric, dict_ref=dict_ref_ic, dict_per=dict_per_c, item='mapq')
     print ('  Looking dict_pc_ric in ref...')
     check_var(dict_ref_ic, dict_pc_ric, ref_index, main_index, alt_index, per=0)
     print ('  Looking dict_pc_ric in per...')
     check_var(dict_per_c, dict_pc_ric, ref_index, main_index, alt_index, per=2)
 
     print ('Num of ref-ic and per-ic =', len(dict_pic_ric))
+    print ('  Looking dict_pic_ric...')
+    check_aln(target=dict_pic_ric, dict_ref=dict_ref_ic, dict_per=dict_per_ic, item='mapq')
     print ('  Looking dict_pic_ric in ref...')
     check_var(dict_ref_ic, dict_pic_ric, ref_index, main_index, alt_index, per=0)
     print ('  Looking dict_pic_ric in per...')
     check_var(dict_per_ic, dict_pic_ric, ref_index, main_index, alt_index, per=2)
 
     print ('Num of ref-ic and per-u =', len(dict_pu_ric))
+    print ('  Looking dict_pu_ric...')
+    check_aln(target=dict_pu_ric, dict_ref=dict_ref_ic, dict_per=None, item='mapq')
     print ('  Looking dict_pu_ric in ref...')
     check_var(dict_ref_ic, dict_pu_ric, ref_index, main_index, alt_index, per=0)
 
     print ('Num of ref-u and per-c =', len(dict_pc_ru))
+    print ('  Looking dict_pc_ru...')
+    check_aln(target=dict_pc_ru, dict_ref=None, dict_per=dict_per_c, item='mapq')
     print ('  Looking dict_pc_ru in per...')
     check_var(dict_per_c, dict_pc_ru, ref_index, main_index, alt_index, per=2)
 
     print ('Num of ref-u and per-ic =', len(dict_pic_ru))
+    print ('  Looking dict_pic_ru...')
+    check_aln(target=dict_pic_ru, dict_ref=None, dict_per=dict_per_ic, item='mapq')
     print ('  Looking dict_pic_ru in per...')
     check_var(dict_per_ic, dict_pic_ru, ref_index, main_index, alt_index, per=2)
 
