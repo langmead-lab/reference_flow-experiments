dca45d0200a9cc07a8996a57a9df5c86a1618ca3
milkschen <nchen18@illinois.edu>
2019-02-06 00:46:08 -0500

    working: finishes building erg, but havent tested

--- scripts/build_varlist_from_vcf.py
+++ scripts/build_varlist_from_vcf.py
@@ -4,7 +4,7 @@ Parses a VCF (V4.2) file and builds a list of variants.
 Also supports the comparison between a golden var list.
 '''
 import argparse
-from build_erg import read_var
+from build_erg import read_var, read_genome
 
 class VCF4_2:
     # CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT
@@ -53,7 +53,150 @@ class VCF4_2:
             else:
                 print ('Error: unexpected variant type :', self.ref_allele, alt_allele)
                 exit ()
+    def print(self, chrm=False, pos=True, ref_allele=True, alt_allele=True):
+        msg = ''
+        if chrm:
+            msg += self.v_chrom
+            msg += ' '
+        if pos:
+            msg += str(self.v_pos)
+            msg += ' '
+        if ref_allele:
+            msg += self.ref_allele
+            msg += ' '
+        if alt_allele:
+            msg += str(self.alt_alleles)
+            msg += ' '
+        print (msg)
+
+def write_erg_wrt_ref(var_list, ref_genome, f_len):
+    '''
+    Write one ERG seq
+    '''
+    erg = ''
+    start_pos = var_list[0].v_pos - f_len
+    end_v = var_list[len(var_list) - 1]
+    
+    end_alt_allele = end_v.alt_alleles[0]
+    # if len(end_v.alt_alleles) > 1:
+    # else:
+    #     end_alt_allele = end_v.alt_alleles
+    end_pos = end_v.v_pos + f_len + len(end_alt_allele)
+    erg_ref = ref_genome[start_pos: end_pos]
+    
+    nxt_start_pos = start_pos
+    for v in var_list:
+        v.print()
+        erg += ref_genome[nxt_start_pos : v.v_pos]
+        erg += v.alt_alleles[0]
+        nxt_start_pos = v.v_pos + len(v.alt_alleles[0])
+    if len(erg) == 0:
+        erg = erg_ref
+    else:
+        erg += ref_genome[nxt_start_pos : end_pos]
+    assert end_pos - start_pos == len(erg)
+    print (start_pos, end_pos)
+    print (erg)
+    input ()
+    return len(erg)
+
+def build_erg_wrt_ref(ref_genome, var_list, args):
+    '''
+    Reads a set of variants under VCF4.2 format and constructs its ERG
+    '''
+    f_len = args.flanking_len
+    tmp_var_list = []
+    num_erg = 0
+    total_len_erg = 0
+
+    for vinfo in var_list:
+        prev_var = 0
+        if len(tmp_var_list) > 0:
+            prev_var = tmp_var_list[len(tmp_var_list) - 1]
+        if len(tmp_var_list) > 0 and \
+            vinfo.v_pos > prev_var.v_pos + 2 * f_len:
+            # write previous vars
+            len_erg = write_erg_wrt_ref(tmp_var_list, ref_genome, f_len)
+            num_erg += 1
+            total_len_erg += len_erg
+            # reset
+            tmp_var_list = [vinfo]
+        elif len(tmp_var_list) > 0:
+            tmp_var_list.append(vinfo)
+        else: # len == 0
+            tmp_var_list = [vinfo]
+    len_erg = write_erg_wrt_ref(tmp_var_list, ref_genome, f_len)
+    num_erg += 1
+    total_len_erg += len_erg
+
+def build_varlist_from_vcf(args):
+    '''
+    ##fileformat=VCFv4.2
+    '''
+    target_vcf_fn = args.vcf
+    golden_vcf_fn = args.golden_vcf
+    min_qual = float(args.min_qual)
+    target_vcf_f = open(target_vcf_fn, 'r')
+    
+    gvar_list = read_var(golden_vcf_fn, remove_conflict=True, remove_coexist=False)
+    var_list = []
+
+    num_vars = 0
+    num_snps = 0
+    num_indels = 0
+    num_true_pos = 0
+
+    golden_idx = 0
+    for line in target_vcf_f:
+        # Skip header lines (##...) and info line (#...)
+        if line[0] == '#':# and line[1] == '#'
+            continue
+        vcf = VCF4_2(line)
+
+        # filtered by QUAL
+        if vcf.v_qual < min_qual:
+            continue
         
+        var_list.append(vcf)
+        
+        for i, alt_allele in enumerate(vcf.alt_alleles):
+            # a diploid variant is counted twice
+            num_vars += 1
+            v_type = vcf.v_type.split(',')[i]
+            if v_type == 'SNP':
+                num_snps += 1
+            elif v_type in ['INS', 'DEL']:
+                num_indels += 1
+            
+            # var_list.append([vcf.v_chrom, v_type, vcf.v_pos, vcf.ref_allele, alt_allele])
+
+            for j in range(golden_idx, len(gvar_list)):
+                if gvar_list[j].chrm == vcf.v_chrom and \
+                gvar_list[j].ref_pos == vcf.v_pos and \
+                gvar_list[j].ref_allele == vcf.ref_allele and \
+                gvar_list[j].alt_allele == alt_allele:
+                    golden_idx = j
+                    num_true_pos += 1
+                    break
+                if gvar_list[j].chrm == vcf.v_chrom and \
+                    gvar_list[j].ref_pos > vcf.v_pos:
+                    golden_idx = max(j - 1, 0)
+                    break
+    
+    # Stats
+    precision = 100 * float(num_true_pos) / num_vars
+    sensitivity = 100 * float(num_true_pos) / len(gvar_list)
+    
+    print ('NUM GOLDEN VARS:', len(gvar_list))
+    # print ('NUM VARS', num_vars)
+    print ('NUM SNPS', num_snps)
+    print ('NUM INDELS', num_indels)
+    print ('NUM TRUE POS', num_true_pos)
+    
+    print ('PRECISION = %.2f%%' % precision)
+    print ('SENSITIVITY = %.2f%%' % sensitivity)
+
+    return var_list
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -69,64 +212,25 @@ def parse_args():
     parser.add_argument(
         '-vg', '--golden_vcf',
         default='',
-        help='golden vcf file'
+        help='golden vcf file ['']'
     )
-
+    parser.add_argument(
+        '-r', '--ref_genome',
+        default='',
+        help='ref genome file ['']'
+    )
+    parser.add_argument(
+        '-f', '--flanking_len', type=int,
+        default=100,
+        help='the length of flanking regions, total length of an erg is 2*f+1 [100]'
+    )
     args = parser.parse_args()
     return args
 
-def build_varlist_from_vcf(args):
-    '''
-    ##fileformat=VCFv4.2
-    '''
-    target_vcf_fn = args.vcf
-    golden_vcf_fn = args.golden_vcf
-    min_qual = float(args.min_qual)
-    target_vcf_f = open(target_vcf_fn, 'r')
-    
-    var_list = read_var(golden_vcf_fn, remove_conflict=True, remove_coexist=False)
-    print ('len of golden list:', len(var_list))
-
-    num_vars = 0
-    num_true_pos = 0
-
-    golden_idx = 0
-    for line in target_vcf_f:
-        # Skip header lines (##...) and info line (#...)
-        if line[0] == '#':# and line[1] == '#'
-            continue
-        vcf = VCF4_2(line)
-
-        # filtered by QUAL
-        if vcf.v_qual < min_qual:
-            continue
-        
-        for i, alt_allele in enumerate(vcf.alt_alleles):
-            # a diploid variant is counted twice
-            num_vars += 1
-            # v_type = vcf.v_type.split(',')[i]
-            # print (vcf.v_chrom, v_type, vcf.v_pos, vcf.ref_allele, alt_allele)
-            for j in range(golden_idx, len(var_list)):
-                if var_list[j].chrm == vcf.v_chrom and \
-                var_list[j].ref_pos == vcf.v_pos and \
-                var_list[j].ref_allele == vcf.ref_allele and \
-                var_list[j].alt_allele == alt_allele:
-                    golden_idx = j
-                    num_true_pos += 1
-                    break
-                if var_list[j].chrm == vcf.v_chrom and \
-                    var_list[j].ref_pos > vcf.v_pos:
-                    golden_idx = max(j - 1, 0)
-                    break
-    
-    # Stats
-    precision = 100 * float(num_true_pos) / num_vars
-    sensitivity = 100 * float(num_true_pos) / len(var_list)
-    
-    print ('PRECISION = %.2f%%' % precision)
-    print ('SENSITIVITY = %.2f%%' % sensitivity)
-    
-
 if __name__ == '__main__':
     args = parse_args()
-    build_varlist_from_vcf(args)
+    var_list = build_varlist_from_vcf(args)
+
+    ref_genome = read_genome(args.ref_genome, None)
+    print ('Warning: ignore HET vars')
+    build_erg_wrt_ref(ref_genome, var_list, args)
