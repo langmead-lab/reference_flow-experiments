ced03c25cda5305b536dd747c7c741052048bc8b
milkschen <s93rm6@gmail.com>
2019-02-19 17:07:18 -0500

    supports 1kg wgs vcf

--- scripts/build_major_allele_ref.sh
+++ scripts/build_major_allele_ref.sh
@@ -1,5 +1,5 @@
 if [ "$#" -ne "3" ]; then
-    echo "Usage: $0 prefix vcf_path index_path" >&3
+    echo "Usage: $0 prefix vcf_path fasta_path" >&3
 else
     msg=$(bcftools -h | wc -l)
     if [ "$msg" -gt "1" ]; then
@@ -8,10 +8,34 @@ else
         echo "loading bcftools...";
         module load bcftools;
     fi
+    msg=$(vcftools -h | wc -l)
+    if [ "$msg" -gt "1" ]; then
+        echo "vcftools has already been loaded";
+    else
+        echo "loading vcftools...";
+        module load vcftools;
+    fi
+    msg=$(samtools -h | wc -l)
+    if [ "$msg" -gt "1" ]; then
+        echo "samtools has already been loaded";
+    else
+        echo "loading samtools...";
+        module load samtools;
+    fi
     PREFIX=$1
     VCF="${PREFIX}.vcf.gz"
+    VCF_R="${PREFIX}.recode.vcf"
+    VCF_R_GZ="${PREFIX}.recode.vcf.gz"
     MA_REF="${PREFIX}_major_allele.fa"
-    bcftools view -O z -v snps,indels --threads 8 -q 0.5 $2 > $VCF
-    bcftools index $VCF
-    bcftools consensus -f $3 $VCF > $MA_REF
+    NUM_TH=16
+    #: bcftools dont solve mnps
+    bcftools view -O z -v snps --threads $NUM_TH -q 0.5 $2 > $VCF
+    #: vcftools need sample to calculate frequency (cannot only look at INFO:AF)
+    # vcftools --gzvcf $2 --non-ref-af 0.5 --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --out $PREFIX --remove-indels
+    # bgzip $VCF -@ $NUM_TH
+    vcftools --gzvcf $VCF --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --out $PREFIX --remove-indels
+    bgzip $VCF_R -@ $NUM_TH
+    bcftools index $VCF_R_GZ
+    bcftools consensus -f $3 $VCF_R_GZ > $MA_REF
+    # samtools faidx $3 {} | bcftools consensus $VCF_GZ > $MA_REF
 fi
