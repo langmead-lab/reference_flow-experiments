df32c199f9527ea3241651aa87ee874cd6d00b90
milkschen <s93rm6@gmail.com>
2019-02-21 00:29:38 -0500

    uses gatk for consensus build; uses sed to fix naming

--- scripts/build_major_allele_ref.sh
+++ scripts/build_major_allele_ref.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne "3" ]; then
-    echo "Usage: $0 prefix vcf_path fasta_path" >&3
+if [ "$#" -ne "4" ]; then
+    echo "Usage: $0 prefix vcf fasta_prefix gatk_path" >&3
 else
     msg=$(bcftools -h | wc -l)
     if [ "$msg" -gt "1" ]; then
@@ -23,26 +23,31 @@ else
         module load samtools;
     fi
     PREFIX=$1
-    VCF="${PREFIX}.vcf.gz"
+    VCF="${PREFIX}.vcf"
+    VCF_GZ="${PREFIX}.vcf.gz"
     VCF_R="${PREFIX}.recode.vcf"
     VCF_R_GZ="${PREFIX}.recode.vcf.gz"
     MA_REF="${PREFIX}_major_allele.fa"
     NUM_TH=16
     #: bcftools dont solve mnps
-    bcftools view -O z -v snps --threads $NUM_TH -q 0.5 $2 > $VCF
+    # bcftools view -O z -v snps --threads $NUM_TH -q 0.5 $2 > $VCF_GZ
+
     #: vcftools need sample to calculate frequency (cannot only look at INFO:AF)
     # vcftools --gzvcf $2 --non-ref-af 0.5 --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --out $PREFIX --remove-indels
     # bgzip $VCF -@ $NUM_TH
 
-    #: [start] this works
-    #vcftools --gzvcf $VCF --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --out $PREFIX --remove-indels
+    #: [start]
+    #vcftools --gzvcf $VCF_GZ --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --out $PREFIX --remove-indels
     #bgzip $VCF_R -@ $NUM_TH
     #bcftools index $VCF_R_GZ
-    #bcftools consensus -f $3 $VCF_R_GZ > $MA_REF
+    #bcftools consensus -f ${3}.fa $VCF_R_GZ > $MA_REF
     #: [end]
 
-    vcftools --gzvcf $VCF --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --stdout --remove-indels | bgzip -@ $NUM_TH > $VCF_R_GZ
+    bcftools view -O v -v snps --threads $NUM_TH -q 0.5 $2 > $VCF_GZ
+    vcftools --gzvcf $VCF_GZ --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --stdout --remove-indels | bgzip -@ $NUM_TH > $VCF_R_GZ
     bcftools index $VCF_R_GZ
-    #bcftools consensus -f $3 $VCF_R_GZ > $MA_REF
-    samtools faidx $3 $1 | bcftools consensus $VCF_GZ > $MA_REF
+    $4 CreateSequenceDictionary -R ${3}.fa -O ${3}.dict
+    $4 IndexFeatureFile -F $VCF_R_GZ
+    $4 FastaAlternateReferenceMaker -R ${3}.fa -O $MA_REF -V $VCF_R_GZ
+    sed -iE 's/>[0-9+]* />/; s/:/ /' $MA_REF
 fi
