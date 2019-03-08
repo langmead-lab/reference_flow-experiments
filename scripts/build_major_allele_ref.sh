79f4130e3cde9235877435686cc0f07fb33fc06f
milkschen <s93rm6@gmail.com>
2019-03-08 02:42:08 -0500

    used bcftools consensus to replace gatk

--- scripts/build_major_allele_ref.sh
+++ scripts/build_major_allele_ref.sh
@@ -15,7 +15,7 @@ else
         echo "loading vcftools...";
         module load vcftools;
     fi
-    msg=$(samtools -h | wc -l)
+    msg=$(samtools view | wc -l)
     if [ "$msg" -gt "1" ]; then
         echo "samtools has already been loaded";
     else
@@ -32,17 +32,19 @@ else
     #: bcftools dont solve mnps
     #: vcftools need sample to calculate frequency (cannot only look at INFO:AF)
     
-    #bcftools view -O v -v snps --threads $NUM_TH -q 0.5 $2 > $VCF_GZ
     bcftools view -O z -v snps --threads $NUM_TH -q 0.5 $2 > $VCF_GZ
     vcftools --gzvcf $VCF_GZ --min-alleles 2 --max-alleles 2 --recode-INFO-all --recode --stdout --remove-indels | bgzip -@ $NUM_TH > $VCF_R_GZ
     bcftools index $VCF_R_GZ
-    $4 CreateSequenceDictionary -R ${3}.fa -O ${3}.dict
-    $4 IndexFeatureFile -F $VCF_R_GZ
-    $4 FastaAlternateReferenceMaker -R ${3}.fa -O $MA_REF -V $VCF_R_GZ
-    sed -iE 's/>[0-9+]* />/; s/:/ /' $MA_REF
+    bcftools consensus -f ${3}.fa $VCF_R_GZ > $MA_REF
+
+    # $4 CreateSequenceDictionary -R ${3}.fa -O ${3}.dict
+    # $4 IndexFeatureFile -F $VCF_R_GZ
+    # $4 FastaAlternateReferenceMaker -R ${3}.fa -O $MA_REF -V $VCF_R_GZ
+    # sed -iE 's/>[0-9+]* />/; s/:/ /' $MA_REF
 
     #: test major allele reference using chromosome vcfs
-    sh $REL/scripts/test_major_allele_ref.sh $MA_REF
+    # sh ../scripts/test_major_allele_ref.sh $MA_REF
+
     # for i in $(seq 1 22) X Y; do
     #     bgzip -cd $GS/1kg_vcf/${i}.vcf.gz | \
     #         python $REL/scripts/vcf_processing.py --out_var_loc 1 --min_af 0.5 --rand_th 1 --max_allele_len 1| \
