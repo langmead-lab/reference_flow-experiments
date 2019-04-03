1ee9865cfcc7da8c964545ca3750cdfeab02efd0
Nae-Chyun <s93rm6@gmail.com>
2019-04-03 19:47:25 -0400

    fixed bug and added h37maj in the plot

--- scripts/plot_from_exps.py
+++ scripts/plot_from_exps.py
@@ -63,21 +63,27 @@ if __name__ == '__main__':
     superpop_fn = args.superpopulation
     
     TP_OFFSET = 19719900 # num of TP in the first pass
-    TP_FLOOR  = 19908912 # num of TP using purely hg19
-    TP_CEIL   = 19929495 # num of TP using personalized genomes
+    TP_H37    = 19908912 # num of TP using purely hg19
+    TP_H37MAJ = 19918604 # num of TP using h37maj (snvs+indels)
+    TP_NA12878= 19929495 # num of TP using personalized genomes
+    NUM_READS = 20000000
 
     list_tp = read_true_pos(log_fn)
     list_samples = read_samples(sample_fn)
     data_df = pd.DataFrame(list_tp)
-    data_df.columns = ['Num TP']
+    data_df.columns = ['Num TP (Second Pass)']
     data_df.index = list_samples
     dict_pop = read_ped(ped_fn)
     list_pop = [dict_pop[data_df.index[i]] for i in range(data_df.shape[0])]
     data_df['Population'] = list_pop
-    data_df['Num TP'] += TP_OFFSET
+    data_df['Num TP'] = data_df['Num TP (Second Pass)'] + TP_OFFSET
 
-    list_tp = [TP_FLOOR] + list(data_df['Num TP']) + [TP_CEIL]
-    list_sample = ['hg19'] + list(data_df.index) + ['personalized']
+    #: special items people would like to compare
+    data_df.loc['h37'] = [TP_H37-TP_OFFSET, 'h37', TP_H37]
+    data_df.loc['NA12878'] = [TP_NA12878-TP_OFFSET, 'NA12878', TP_NA12878]
+    data_df.loc['h37maj'] = [TP_H37MAJ-TP_OFFSET, 'h37maj', TP_H37MAJ]
+    
+    data_df['Sensitivity'] = data_df['Num TP'] / NUM_READS
 
     superpop_df = pd.read_csv(superpop_fn, sep='\t', header=None, index_col=0)
     superpop_groups = superpop_df.groupby(2)
@@ -85,19 +91,19 @@ if __name__ == '__main__':
     for n, g in superpop_groups:
         for i in superpop_groups.groups[n]:
             dict_superpop[i] = n
-    dict_pop['hg19'] = 'hg19'
-    dict_pop['personalized'] = 'CEU'
-    dict_superpop['hg19'] = 'hg19'
+    dict_pop['h37'] = 'h37'
+    dict_pop['h37maj'] = 'h37maj'
+    dict_superpop['h37'] = 'h37'
+    dict_superpop['h37maj'] = 'h37maj'
+    dict_color = {'AFR': 'C0', 'AMR': 'C1', 'CEU': 'C2', 'EAS': 'C3', 'EUR': 'C4', 'SAS': 'C5', 'h37': 'C6', 'h37maj': 'C7'}
 
-    dict_color = {'AFR': 'C0', 'AMR': 'C1', 'CEU': 'C2', 'EAS': 'C3', 'EUR': 'C4', 'SAS': 'C5', 'hg19': 'C6'}
-
-    list_tp.sort()
-    x = np.arange(len(list_tp))
-    bar = plt.bar(x, list_tp)
-    plt.ylim(bottom=1.1*min(list_tp)-0.1*max(list_tp), top=max(list_tp))
+    data_df = data_df.sort_values(by='Sensitivity')
+    x = np.arange(data_df.shape[0])
+    bar = plt.bar(x, data_df['Sensitivity'])
+    plt.ylim(bottom=1.1*min(data_df['Sensitivity'])-0.1*max(data_df['Sensitivity']), top=max(data_df['Sensitivity']))
     existed_superpop = []
-    for i in range(len(list_sample)):
-        pop = dict_pop[list_sample[i]]
+    for i in range(data_df.shape[0]):
+        pop = dict_pop[data_df.index[i]]
         superpop = dict_superpop[pop]
         if superpop not in existed_superpop:
             existed_superpop.append(superpop)
@@ -105,6 +111,7 @@ if __name__ == '__main__':
         color = dict_color[superpop]
         bar[i].set_color(color)
     plt.legend()
-    plt.title('Aligned to h37maj and then 100 random indivs; mapq_th=10')
+    plt.ylabel('Sensitivity')
+    plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10')
     plt.show()
     plt.clf()
