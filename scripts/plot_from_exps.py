0234d354e9a0ee66bd64e0668a0ab8ba6361dae5
Nae-Chyun <s93rm6@gmail.com>
2019-04-09 16:13:35 -0400

    supported boxplot

--- scripts/plot_from_exps.py
+++ scripts/plot_from_exps.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -62,11 +63,22 @@ if __name__ == '__main__':
     ped_fn = args.ped
     superpop_fn = args.superpopulation
     
-    TP_OFFSET = 19719900 # num of TP in the first pass
-    TP_H37    = 19908912 # num of TP using purely hg19
-    TP_H37MAJ = 19918604 # num of TP using h37maj (snvs+indels)
-    TP_NA12878= 19929495 # num of TP using personalized genomes
+    ''' NA12878 '''
+    # TP_OFFSET = 0
+    # TP_OFFSET = 19719900 # num of TP in the first pass h37maj
+    # TP_H37    = 19908912 # num of TP using purely h37
+    # TP_H37MAJ = 19918604 # num of TP using h37maj (snvs+indels)
+    # TP_PER    = 19929495 # num of TP using personalized genomes
+    
+    ''' HG03518 '''
+    # TP_OFFSET = 0 # num of TP in the first pass h37maj
+    TP_OFFSET = 19706590 # num of TP in the first pass h37maj
+    TP_H37    = 19903321 # num of TP using purely h37
+    TP_H37MAJ = 19913067 # num of TP using h37maj (snvs+indels)
+    TP_PER    = 19929234 # num of TP using personalized genomes
+    
     NUM_READS = 20000000
+    
 
     list_tp = read_true_pos(log_fn)
     list_samples = read_samples(sample_fn)
@@ -80,7 +92,7 @@ if __name__ == '__main__':
 
     #: special items people would like to compare
     data_df.loc['h37'] = [TP_H37-TP_OFFSET, 'h37', TP_H37]
-    data_df.loc['NA12878'] = [TP_NA12878-TP_OFFSET, 'NA12878', TP_NA12878]
+    data_df.loc['personalized'] = [TP_PER-TP_OFFSET, 'personalized', TP_PER]
     data_df.loc['h37maj'] = [TP_H37MAJ-TP_OFFSET, 'h37maj', TP_H37MAJ]
     
     data_df['Sensitivity'] = data_df['Num TP'] / NUM_READS
@@ -93,25 +105,60 @@ if __name__ == '__main__':
             dict_superpop[i] = n
     dict_pop['h37'] = 'h37'
     dict_pop['h37maj'] = 'h37maj'
+    dict_pop['personalized'] = 'personalized'
     dict_superpop['h37'] = 'h37'
     dict_superpop['h37maj'] = 'h37maj'
-    dict_color = {'AFR': 'C0', 'AMR': 'C1', 'CEU': 'C2', 'EAS': 'C3', 'EUR': 'C4', 'SAS': 'C5', 'h37': 'C6', 'h37maj': 'C7'}
+    dict_superpop['personalized'] = 'personalized'
+    dict_color = {'AFR': 'C0', 'AMR': 'C1', 'CEU': 'C2', 'EAS': 'C3', 'EUR': 'C4', 'SAS': 'C5', 'h37': 'C6', 'h37maj': 'C7', 'personalized': 'C8'}
 
     data_df = data_df.sort_values(by='Sensitivity')
-    x = np.arange(data_df.shape[0])
-    bar = plt.bar(x, data_df['Sensitivity'])
-    plt.ylim(bottom=1.1*min(data_df['Sensitivity'])-0.1*max(data_df['Sensitivity']), top=max(data_df['Sensitivity']))
-    existed_superpop = []
-    for i in range(data_df.shape[0]):
-        pop = dict_pop[data_df.index[i]]
-        superpop = dict_superpop[pop]
-        if superpop not in existed_superpop:
-            existed_superpop.append(superpop)
-            bar[i].set_label(superpop)
-        color = dict_color[superpop]
-        bar[i].set_color(color)
-    plt.legend()
-    plt.ylabel('Sensitivity')
-    plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10')
+    list_superpop = []
+    for i in data_df['Population']:
+        list_superpop.append(dict_superpop[i])
+    list_superpop
+    data_df['Superpopulation'] = list_superpop
+
+    #: sort categories by the median of each superpop
+    order_by_superpop = list(data_df.groupby('Superpopulation').median().sort_values('Sensitivity').index)
+
+    ''' Bar plot '''
+    # x = np.arange(data_df.shape[0])
+    # bar = plt.bar(x, data_df['Sensitivity'])
+    # plt.ylim(bottom=1.1*min(data_df['Sensitivity'])-0.1*max(data_df['Sensitivity']), top=max(data_df['Sensitivity']))
+    # existed_superpop = []
+    # for i in range(data_df.shape[0]):
+    #     pop = dict_pop[data_df.index[i]]
+    #     superpop = dict_superpop[pop]
+    #     if superpop not in existed_superpop:
+    #         existed_superpop.append(superpop)
+    #         bar[i].set_label(superpop)
+    #     color = dict_color[superpop]
+    #     bar[i].set_color(color)
+    # plt.legend()
+    # plt.ylabel('Sensitivity')
+    # plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10 (NA12878)')
+    # # plt.title('First pass: h37maj; second pass: 100 random indivs (hap); mapq_th=10 (NA12878)')
+    # # plt.title('Aligned against 100 random indivs (NA12878)')
+    # # plt.title('Aligned against 100 random indivs (HG03518)')
+    # # plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10 (HG03518)')
+    # plt.show()
+    # plt.clf()
+
+    ''' Box plot '''
+    # sns.boxplot(x='Population', y='Sensitivity', hue='Superpopulation', data=data_df)
+    # sns.swarmplot(x='Population', y='Sensitivity', hue='Superpopulation', data=data_df)
+    sns.boxplot(x='Superpopulation', y='Sensitivity', data=data_df, order=order_by_superpop)
+    sns.swarmplot(x='Superpopulation', y='Sensitivity', data=data_df, order=order_by_superpop)
+    # plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10 (NA12878)')
+    # plt.title('First pass: h37maj; second pass: 100 random indivs (hap); mapq_th=10 (NA12878)')
+    # plt.title('Aligned against 100 random indivs (NA12878)')
+    # plt.title('Aligned against 100 random indivs (HG03518)')
+    plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10 (HG03518)')
     plt.show()
     plt.clf()
+
+    # sns.barplot(x='Population', y='Sensitivity', hue='Superpopulation', data=data_df)
+    # plt.ylim(bottom=1.1*min(data_df['Sensitivity'])-0.1*max(data_df['Sensitivity']), top=max(data_df['Sensitivity']))
+    # plt.title('First pass: h37maj; second pass: 100 random indivs; mapq_th=10 (NA12878)')
+    # plt.show()
+    # plt.clf()
