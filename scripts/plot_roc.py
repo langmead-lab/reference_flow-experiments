7cebbc892f61365f1cba56c5b1fcf62ea840456d
Nae-Chyun <s93rm6@gmail.com>
2019-04-18 02:04:48 -0400

    supported chr9

--- scripts/plot_roc.py
+++ scripts/plot_roc.py
@@ -3,13 +3,19 @@ import matplotlib.pyplot as plt
 
 def get_tpr_fdr(df, n_var):
     g_nv = df.groupby('numvar').groups
-    num_total = g_nv[n_var].shape[0]
+    if n_var == None:
+        num_total = df.shape[0]
+    else:
+        num_total = g_nv[n_var].shape[0]
     
     g_q = df.groupby('mapq').groups
     list_tp = []
     list_fd = []
-    for qual in g_q:
-        cnt = df.iloc[g_q[qual] & g_nv[n_var]]['dist'].value_counts()
+    for qual in list(g_q.keys())[::-1]:
+        if n_var == None:
+            cnt = df.iloc[g_q[qual]]['dist'].value_counts()
+        else:
+            cnt = df.iloc[g_q[qual] & g_nv[n_var]]['dist'].value_counts()
         if len(list_tp) > 0:
             num_tp = list_tp[-1]
         else:
@@ -18,7 +24,6 @@ def get_tpr_fdr(df, n_var):
         for i in range(0, 11):
             if i in cnt:
                 num_tp_in_group += cnt[i]
-        # print (num_tp_in_group)
         try:
             if len(list_fd) > 0:
                 num_fd = sum(cnt) - cnt[-3] - num_tp_in_group + list_fd[-1]
@@ -32,9 +37,6 @@ def get_tpr_fdr(df, n_var):
 
         list_tp.append(num_tp + num_tp_in_group)
         list_fd.append(num_fd)
-    
-    # tpr = num_tp / num_total
-    # fdr = num_fd / num_total
 
     list_tpr = [i / num_total for i in list_tp]
     list_fdr = [i / num_total for i in list_fd]
@@ -43,15 +45,21 @@ def get_tpr_fdr(df, n_var):
     print (list_fdr)
     return list_tpr, list_fdr
 
-df_ref = pd.read_pickle('10M-ref.sam-stats.pkl')
-df_h37maj = pd.read_pickle('10M-h37maj.sam-stats.pkl')
-df_per = pd.read_pickle('10M-per.sam-stats.pkl')
+# df_ref = pd.read_pickle('10M-ref.sam-stats.pkl')
+# df_h37maj = pd.read_pickle('10M-h37maj.sam-stats.pkl')
+# df_per = pd.read_pickle('10M-per.sam-stats.pkl')
 
-list_num_var = [0, 1, 2, 3]
+df_ref = pd.read_pickle('chr9-10M-ref.sam-stats.pkl')
+df_h37maj = pd.read_pickle('chr9-10M-h37maj.sam-stats.pkl')
+df_per = pd.read_pickle('chr9-10M-per.sam-stats.pkl')
+
+
+# list_num_var = [0, 1, 2, 3]
+list_num_var = [None]
 plt.figure(1)
 for i, num_var in enumerate(list_num_var):
-    # num_var = 2
-    plt.subplot(2, 2, i+1)
+    # plt.subplot(2, 2, i+1)
+    plt.subplot(1, 1, i+1)
     list_tpr_ref, list_fdr_ref = get_tpr_fdr(df_ref, num_var)
     list_tpr_h37maj, list_fdr_h37maj = get_tpr_fdr(df_h37maj, num_var)
     list_tpr_per, list_fdr_per = get_tpr_fdr(df_per, num_var)
@@ -63,6 +71,8 @@ for i, num_var in enumerate(list_num_var):
     plt.ylabel('TPR (sensitivity)')
     plt.xlabel('FDR (1-precision)')
     plt.legend()
-    plt.title('Chr21, number of vars = {0}'.format(num_var))
+
+    # plt.title('Chr21, number of vars = {0}'.format(num_var))
+    plt.title('Chr9, number of vars = {0}'.format(num_var))
 plt.show()
 plt.clf()
