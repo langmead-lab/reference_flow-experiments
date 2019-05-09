2bba1f4a391957e7725dc11b177cbdc09cc13f54
Nae-Chyun <s93rm6@gmail.com>
2019-05-09 17:22:02 -0400

    finds tie-breakers

--- scripts/analyze_multimappers.py
+++ scripts/analyze_multimappers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 from analyze_sam import SamInfo, parse_line, load_golden_dic
-from analyze_diploid_indels import build_index, count_overlapping_vars, diploid_compare, build_all_indexes
+from analyze_diploid_indels import build_index, diploid_compare, build_all_indexes
 from build_erg import read_var
 
 def check_accuracy_by_dist(dist, threshold):
@@ -9,37 +9,77 @@ def check_accuracy_by_dist(dist, threshold):
         return True
     return False
 
+def count_overlapping_vars(name, info, g_info, main_index, alt_index, MAIN_CHRM, ALT_CHRM, read_len):
+    '''
+    For an alignment, count the number of overlapping variants.
+    Counting is based on raw read (look up golden dictionary)
+    '''
+    num_var = 0   
+    for i in range(g_info.pos, g_info.pos + read_len):
+        if g_info.chrm == MAIN_CHRM:
+            if main_index.get(i) != None:
+                num_var += 1
+        elif g_info.chrm == ALT_CHRM:
+            if alt_index.get(i) != None:
+                num_var += 1
+        else:
+            print ('Error: unexpected chrm', info.chrm)
+            info.print()
+            exit()
+    return num_var
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-stats', '--stats',
+    '-stats', '--fn_stats',
     help='target stats file (-stats.pkl)'
 )
 parser.add_argument(
-    '-sam', '--sam',
+    '-sam', '--fn_sam',
     help='target sam file'
 )
 parser.add_argument(
     '-g', '--fn_golden',
     help='ground truth sam file'
 )
+# parser.add_argument(
+#     '-v', '--var',
+#     help='.var file specifying variants included in the genome'
+# )
 parser.add_argument(
-    '-v', '--var',
+    '-vr', '--fn_var_reads',
     help='.var file specifying variants included in the genome'
+)
+parser.add_argument(
+    '-vs', '--fn_var_sample',
+    help='.var file specifying variants included in the genome'
+)
+parser.add_argument(
+    '-o', '--fn_out',
+    help='name of output log file'
 )
 parser.add_argument(
     '-read_len', '--read_len', type=int, default=100,
     help='read length (100)'
 )
 parser.add_argument(
+    '-step', '--step', type=int, default=1000,
+    help='step size (1000)'
+)
+parser.add_argument(
     '-t', '--threshold', type=int, default=10,
     help='threshold for comparison (10)'
 )
 args = parser.parse_args()
-fn_stats = args.stats
+fn_stats = args.fn_stats
 fn_golden = args.fn_golden
-fn_sam = args.sam
-fn_var = args.var
+fn_out = args.fn_out
+fn_sam = args.fn_sam
+fn_var_sample = args.fn_var_sample
+fn_var_reads = args.fn_var_reads
+# fn_var = args.var
 read_len = args.read_len
+step = args.step
 threshold = args.threshold
 df = pd.read_pickle(fn_stats)
 
@@ -82,16 +122,21 @@ assert num_total == (num_correct + num_incorrect)
 print ('Sensitivity = {0:.4%} ( {1} / {2} )'.format(num_correct/num_total, num_correct, num_total))
 
 #: reads variants and builds index
-var_reads_list = read_var(fn_var, remove_conflict=True, remove_coexist=False)
+# var_reads_list = read_var(fn_var, remove_conflict=True, remove_coexist=False)
 all_indexes = build_all_indexes(
-    var_reads_fn=fn_var,
-    var_sample_fn=fn_var,
+    var_reads_fn=fn_var_reads,
+    var_sample_fn=fn_var_sample,
     personalized=2,
-    step=1000,
+    step=step,
     MAIN_STRAND=MAIN_STRAND,
     ALT_STRAND=ALT_STRAND
 )
-main_index, alt_index, reads_main_offset_index, reads_alt_offset_index, sample_main_offset_index, sample_alt_offset_index = all_indexes
+
+# main_index, alt_index, reads_main_offset_index, reads_alt_offset_index, sample_main_offset_index, sample_alt_offset_index = all_indexes
+_, _, reads_main_offset_index, reads_alt_offset_index, sample_main_offset_index, sample_alt_offset_index = all_indexes
+var_reads_list = read_var(fn_var_sample, remove_conflict=True, remove_coexist=False)
+main_index, alt_index = build_index(var_reads_list, MAIN_STRAND=MAIN_STRAND, ALT_STRAND=ALT_STRAND)
+
 
 dict_sam_incorrect = {}
 dict_sam_correct = {}
@@ -113,25 +158,32 @@ for line in f_sam:
     #: one of the multi-mappers is correct
     if name in dict_sam_correct:
         score = dict_sam_correct[name][0].score
-        if info.score >= score:
-            dict_sam_correct[name].append(info)
+        dict_sam_correct[name].append(info)
+        # if info.score >= score:
+        #     dict_sam_correct[name].append(info)
     else:
         dict_sam_correct[name] = [info]
 
 golden_dic = load_golden_dic(fn_golden, 1)
 
-f_out = open('per-k3-tiedAS-and-correct.log', 'w')
+df_out = pd.DataFrame(
+    columns=['name', 'pos', 'chrom', 'score', 'num_var', 'dist', 'correctness', 'tie_broken']
+)
 dict_overlapping_var_correct = {}
 for key in list(dict_sam_correct.keys()):
     if len(dict_sam_correct[key]) == 1:
         continue
     var_flag = False
-    tmp_info = []
+    df_tmp = pd.DataFrame(
+        columns=['name', 'pos', 'chrom', 'score', 'num_var', 'dist', 'correctness', 'tie_broken']
+    )
     for i, info in enumerate(dict_sam_correct[key]):
+        #: counts the number of overlapping variants for each aligned region
         num_var = count_overlapping_vars(
             name = key,
             info = info,
             g_info = info,
+            # g_info = golden_dic[key],
             main_index = main_index,
             alt_index = alt_index,
             MAIN_CHRM = MAIN_CHRM,
@@ -145,25 +197,27 @@ for key in list(dict_sam_correct.keys()):
             name = key, 
             threshold = threshold, 
             dip_flag = 'same_id', 
-            step = 1000,
-            COMPARE_SEQ=COMPARE_SEQ,
+            step = step,
+            COMPARE_SEQ = COMPARE_SEQ,
             reads_main_offset_index = reads_main_offset_index, 
             reads_alt_offset_index = reads_alt_offset_index,
             sample_main_offset_index = sample_main_offset_index,
             sample_alt_offset_index = sample_alt_offset_index,
-            MAIN_CHRM=MAIN_CHRM,
-            ALT_CHRM=ALT_CHRM,
-            MAIN_HAP=MAIN_HAP,
-            ALT_HAP=ALT_HAP
+            MAIN_CHRM = MAIN_CHRM,
+            ALT_CHRM = ALT_CHRM,
+            MAIN_HAP = MAIN_HAP,
+            ALT_HAP = ALT_HAP
         )
         if dist >= 0 and dist <= threshold:
-            tmp = '{0}  {1:10d}  {2}  {3}  {4}  {5}  {6}'.format(key, info.pos, info.chrm, info.score, num_var, dist, '*')
+            comp = 1
         else:
-            tmp = '{0}  {1:10d}  {2}  {3}  {4}  {5}  {6}'.format(key, info.pos, info.chrm, info.score, num_var, dist, '')
-        tmp_info.append(tmp)
+            comp = 0
+        tmp = pd.DataFrame([[key, info.pos, info.chrm, info.score, num_var, dist, comp, 0]], columns=['name','pos','chrom','score','num_var','dist','correctness', 'tie_broken'])
+        df_tmp = df_tmp.append(tmp)
     if var_flag:
-        for t in tmp_info:
-            print (t)
-            f_out.write(t+'\n')
-        # print (golden_dic[key].pos)
-        # input ()
+        df_tmp = df_tmp.sort_values('score', ascending=False)
+        best_score = max(df_tmp['score'])
+        if df_tmp.iloc[1]['score'] < best_score:
+            df_tmp['tie_broken'].iloc[0] = 1
+        df_out = df_out.append(df_tmp)
+    df_out.to_csv(fn_out, sep='\t', index=None)
