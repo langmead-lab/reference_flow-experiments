68fc1ffecc0563adf8b8510ea605592b6046d86e
Nae-Chyun <s93rm6@gmail.com>
2019-05-19 22:25:35 -0400

    handles MD and CIGAR with indels

--- scripts/calculate_as_from_simulation.py
+++ scripts/calculate_as_from_simulation.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import numpy as np
 import argparse
 import re
 import math
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-sam', '--fn_sam',
+    '-n', '--fn_sam',
     help='target sam file'
+)
+parser.add_argument(
+    '-g', '--fn_gold',
+    help='golden .fq.sam file'
 )
 parser.add_argument(
     '-read_len', '--read_len', type=int, default=100,
@@ -14,25 +19,28 @@ parser.add_argument(
 )
 args = parser.parse_args()
 read_len = args.read_len
+fn_gold = args.fn_gold
 fn_sam = args.fn_sam
 
-df = pd.read_csv(fn_sam, sep='\t', header=None)
-
-# processes CIGAR string
-split_cigar = []
-for i in range(df.shape[0]):
-    split_cigar.append(re.findall('([0-9]+[a-zA-Z]+)', df[5].iloc[i]))
-df['cigar'] = split_cigar
+df = pd.read_csv(fn_gold, sep='\t', header=None)
+df.columns = ['QNAME', 'FLAG', 'RNAME', 'POS', 'MAPQ', 'CIGAR', 'RNEXT', 'PNEXT', 'TLEN', 'SEQ', 'QUAL', 'NM', 'MD', 'XE', 'XS', 'XI']
 
 # processes MD string
 split_md = []
 for i in range(df.shape[0]):
-    split_md.append(re.findall('([0-9]+|[a-zA-Z]+)', df[12].iloc[i].split(':')[-1]))
-df['md'] = split_md
+    split_md.append(re.findall('([0-9]+|[a-zA-Z]+)', df['MD'].iloc[i].split(':')[-1]))
+df['sMD'] = split_md
+
+# processes CIGAR string
+split_cigar = []
+for i in range(df.shape[0]):
+    split_cigar.append(re.findall('([0-9]+[a-zA-Z]+)', df['CIGAR'].iloc[i]))
+# df['cigar'] = split_cigar
 
 reduced_cigar = []
 for i in range(df.shape[0]):
-    ci = df['cigar'][i]
+    # ci = df['cigar'][i]
+    ci = split_cigar[i]
     for c in ci:
         if c.count('P') > 0:
             ci.remove(c)
@@ -57,34 +65,68 @@ for i in range(df.shape[0]):
             len_ci += int(c[:-1])
     assert len_ci == read_len
     reduced_cigar.append(ci)
-df['reduced_cigar'] = reduced_cigar
+df['rCIGAR'] = reduced_cigar
 
 list_as = [None] * df.shape[0]
 for i in range(df.shape[0]):
     #: reads with no error
-    if int(df[13].iloc[i].split(':')[-1]) == 0:
+    if int(df['XE'].iloc[i].split(':')[-1]) == 0:
         list_as[i] = 0
     else:
         score = 0
-        for c in df['reduced_cigar'].iloc[i]:
-            if c[-1] in ['I', 'D']:
+        vec_aln = []
+        for c in df['rCIGAR'].iloc[i]:
+            #: "0" = match/mismatch
+            if c[-1] == 'M':
+                for z in range(int(c[:-1])):
+                    vec_aln.append(0)
+            elif c[-1] in ['I', 'D']:
                 len_indel = int(c[:-1])
                 score -= (5 + 3 * len_indel)
+                #: "1" = insertion
+                if c[-1] == 'I':
+                    for z in range(int(c[:-1])):
+                        vec_aln.append(1)
+                #: "2" = deletion
+                else:
+                    for z in range(int(c[:-1])):
+                        vec_aln.append(2)
         current_pos = 0
-        for c in df['md'].iloc[i]:
+        current_count_del = 0
+        current_cigar_pos = current_pos + current_count_del
+        for c in df['sMD'].iloc[i]:
             if c.isalpha():
                 for a in c:
-                    qual = df[10].iloc[i][current_pos]
-                    qual = ord(qual) - 33
-                    score -= (2 + math.floor(4 * min(qual, 40)/40))
-                    current_pos += 1
-                #: ignores QUAL
-                # score -= (len(c) * 6)
-                # current_pos += len(c)
+                    if vec_aln[current_cigar_pos] == 2:
+                        current_count_del += 1
+                        # print ('D at {}'.format(current_pos))
+                    else:
+                        qual = df['QUAL'].iloc[i][current_pos]
+                        qual = ord(qual) - 33
+                        score -= (2 + math.floor(4 * min(qual, 40)/40))
+                        current_pos += 1
+                    current_cigar_pos = current_pos + current_count_del
             else:
-                current_pos += int(c)
+                #: checks INS
+                overlapping_ins = vec_aln[current_cigar_pos: current_cigar_pos + int(c) + 1].count(1)
+                if overlapping_ins > 0:
+                    overlapping_ins = vec_aln[current_cigar_pos: current_cigar_pos + overlapping_ins + int(c) + 1].count(1)
+                current_pos += int(c) + overlapping_ins
+            current_cigar_pos = current_pos + current_count_del
+        assert current_pos == read_len
         list_as[i] = score
 df['AS'] = list_as
 
-#: TODO
-#: comparison with another sam file
+#: TODO: tags may differ, need parse line by line
+#: compares with another sam file
+df_sam = pd.read_csv(fn_sam, sep='\t', header=None)
+df_sam_mapq_and_score = df_sam[[0,4,11,12]]
+df_sam_mapq_and_score.columns = ['QNAME', 'MAPQ', 'AS', 'XS']
+list_align_as = [int(i.split(':')[-1]) for i in df_sam_mapq_and_score['AS']]
+df_sam_mapq_and_score['AS'] = list_align_as
+list_align_xs = [int(i.split(':')[-1]) for i in df_sam_mapq_and_score['XS']]
+df_sam_mapq_and_score['XS'] = list_align_xs
+df_merge = df.merge(df_sam_mapq_and_score, on='QNAME')
+
+print (sum(df_merge['AS_x'] <= df_merge['AS_y']))
+print (sum(df_merge['AS_x'] > df_merge['AS_y']))
