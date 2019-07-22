a456f252d9e333b0ddb310765fa54ef5c5a08488
milkschen <s93rm6@gmail.com>
2019-07-22 15:48:30 -0400

    tests generated vcf file by update_genome

--- tests/test_update_genome.sh
+++ tests/test_update_genome.sh
@@ -18,10 +18,22 @@ else
     echo ".var -- FAILED"
     exit
 fi
+grep -v '##' test_NA12878.vcf | cut -f 1-7 > test_NA12878_v.vcf
+if cmp -s "gold.vcf" "test_NA12878_v.vcf"; then
+    echo ".vcf -- PASS"
+else
+    echo ".vcf -- FAILED"
+    exit
+fi
 
 echo "------------------------------"
 echo "test_update_genome has PASSED!"
-read -p "Delete generated test files? [y/N]:" if_del
+#ls test_NA12878*
+read -p "Delete generated test files?
+--
+`ls test_NA12878*`
+--
+[y/N] " if_del
 case $if_del in
     [Yy]* ) rm test_NA12878*; break;;
     * ) exit;;
