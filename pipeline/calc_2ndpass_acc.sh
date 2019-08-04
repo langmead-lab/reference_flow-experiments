6fd3584a86318e611853838339de74efe8d824c9
milkschen <s93rm6@gmail.com>
2019-08-04 19:54:02 -0400

    supports chromosome other than chr21, still need improvement in convenience of usage

--- pipeline/calc_2ndpass_acc.sh
+++ pipeline/calc_2ndpass_acc.sh
@@ -1,16 +1,19 @@
 REL="$REL"
-VAR="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline"
+# VAR="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline"
+VAR="/storage2/naechyun/refflow/chr9/pipeline"
 #POP="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/experiments/stochastic_seed0"
 #GOLD="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/reads_mason2/NA12878_1-h37maj-mapql10.sam"
 #EXP="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/experiments/exp_seed0"
 GOLD=$1
 POP=$2
+CHROM=$3
+SAMPLE="NA12878"
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
-    echo "ERROR: incorrect number of arguments (should be 2)"
+    echo "ERROR: incorrect number of arguments (should be 3)"
     echo "Usage:"
-    echo "$(basename $0) GOLD POP_VAR_DIR"
+    echo "$(basename $0) GOLD POP_VAR_DIR CHROM"
     exit
 fi
 
@@ -30,8 +33,8 @@ WC=`wc -l merging.ids | cut -d ' ' -f1`
 ARR=(`tail -$(($WC+1)) tmp_merge.log | head -$(($WC))`)
 for i in $(seq 0 4)
 do
-    TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c 21 -g $GOLD -p 0 -vr $VAR/NA12878.var -vs $POP/*${array[$i]}*.var -n ${ARR[$i]} | grep 'sensitivity_all'`
-    # TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c 21 -g $GOLD -p 0 -vr $VAR/NA12878.var -vs $POP/21_superpop_"${array[$i]}"_thrsd0_stochastic.var -n ${ARR[$i]} | grep 'sensitivity_all'`
+    TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c $CHROM -g $GOLD -p 0 -vr $VAR/${SAMPLE}.var -vs $POP/*${array[$i]}*.var -n ${ARR[$i]} | grep 'sensitivity_all'`
+    # TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c $CHROM -g $GOLD -p 0 -vr $VAR/${SAMPLE}.var -vs $POP/${CHROM}_superpop_"${array[$i]}"_thrsd0_stochastic.var -n ${ARR[$i]} | grep 'sensitivity_all'`
     update
 done
 
