46ab4a043c88455dab239e64c0213ed002091370
milkschen <s93rm6@gmail.com>
2019-08-05 15:17:54 -0400

    uses getopts

--- pipeline/calc_2ndpass_acc.sh
+++ pipeline/calc_2ndpass_acc.sh
@@ -1,20 +1,92 @@
-REL="$REL"
+usage(){
+    echo "Usage: $(basename $0) [-CpS] -c chrom -g gold_sam -V path_var_reads -P path_var_genome"
+    echo "------ Requirements -----"
+    echo "  -c  name of chromosome"
+    echo "  -g  gold SAM file provided by simulator"
+    echo "  -V  path of VAR for simulated individual (reads)"
+    echo "  -P  path of VAR for genomes to be aligned"
+    echo "------ Options -----"
+    echo "  -C  category {superpop/pop} [superpop]"
+    echo "  -p  number of threads [8]"
+    echo "  -S  path of scripts ['$'R]"
+    exit
+}
+
+SCRIPTS="/scratch/groups/blangme2/naechyun/relaxing/scripts"
+THREADS=8
+CAT="superpop"
+
 # VAR="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline"
-VAR="/storage2/naechyun/refflow/chr9/pipeline"
+# VAR="/storage2/naechyun/refflow/chr9/pipeline"
 #POP="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/experiments/stochastic_seed0"
 #GOLD="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/reads_mason2/NA12878_1-h37maj-mapql10.sam"
-#EXP="/net/langmead-bigmem-ib.bluecrab.cluster/storage/naechyun/relaxation/chr21/pipeline/experiments/exp_seed0"
-GOLD=$1
-POP=$2
-CHROM=$3
-SAMPLE="NA12878"
 
-if [ "$#" -ne 3 ]
+while getopts c:g:V:P:C:p:S:h: option
+do
+    case "${option}"
+    in
+    #: options
+    C) 
+        echo "Set category -> $CAT"
+        CAT=${OPTARG}
+        ;;
+    p) 
+        echo "Set number of threads -> $THREADS"
+        THREADS=${OPTARG}
+        ;;
+    S) 
+        echo "Set script directory -> $SCRIPTS"
+        SCRIPTS=${OPTARG}
+        ;;
+
+    #: requirements
+    c) CHROM=${OPTARG};;
+    g) GOLD=${OPTARG};;
+    V) VAR=${OPTARG};;
+    P) POP=${OPTARG};;
+
+    #: help
+    h)
+        usage;;
+    *)
+        usage
+    esac
+done
+
+#: check if required fields are given
+if [ -z ${CHROM+x} ]
 then
-    echo "ERROR: incorrect number of arguments (should be 3)"
-    echo "Usage:"
-    echo "$(basename $0) GOLD POP_VAR_DIR CHROM"
-    exit
+    echo "error: required input chrom (-c) is not set"
+    usage
+fi
+if [ -z ${GOLD+x} ]
+then
+    echo "error: required input gold (-g) is not set"
+    usage
+fi
+if [ -z ${VAR+x} ]
+then
+    echo "error: required input var (-V) is not set"
+    usage
+fi
+if [ -z ${POP+x} ]
+then
+    echo "error: required input population var (-P) is not set"
+    usage
+fi
+
+#: set population
+if [ $CAT == "pop" ]
+then
+    array=( "ACB" "ASW" "BEB" "CDX" "CEU" "CHB" "CHS" "CLM" "ESN" "FIN" "GBR" "GIH" "GWD" "IBS" "ITU" "JPT" "KHV" "LWK" "MSL" "MXL" "PEL" "PJL" "PUR" "STU" "TSI" "YRI" )
+elif [ $CAT == "superpop" ]
+then
+    #: for testing purpose
+    #array=( "EUR" )
+    array=( "EUR" "AMR" "SAS" "AFR" "EAS" )
+else
+    echo "ERROR: invalid input: $CAT"
+    usage
 fi
 
 TP=0
@@ -28,12 +100,15 @@ update(){
     echo "SUM=$SUM"
 }
 
-array=( "EUR" "AMR" "SAS" "AFR" "EAS" )
 WC=`wc -l merging.ids | cut -d ' ' -f1`
 ARR=(`tail -$(($WC+1)) tmp_merge.log | head -$(($WC))`)
 for i in $(seq 0 4)
 do
-    TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c $CHROM -g $GOLD -p 0 -vr $VAR/${SAMPLE}.var -vs $POP/*${array[$i]}*.var -n ${ARR[$i]} | grep 'sensitivity_all'`
+    TMP=`python3.7 -O $SCRIPTS/analyze_diploid_indels.py \
+        -c $CHROM -g $GOLD -p 0 -vr $VAR \
+        -vs $POP/${CHROM}_${CAT}_${array[$i]}*.var -n ${ARR[$i]} \
+        | grep 'sensitivity_all'`
+    # TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c $CHROM -g $GOLD -p 0 -vr $VAR/${SAMPLE}.var -vs $POP/*${array[$i]}*.var -n ${ARR[$i]} | grep 'sensitivity_all'`
     # TMP=`python3.7 -O $REL/scripts/analyze_diploid_indels.py -c $CHROM -g $GOLD -p 0 -vr $VAR/${SAMPLE}.var -vs $POP/${CHROM}_superpop_"${array[$i]}"_thrsd0_stochastic.var -n ${ARR[$i]} | grep 'sensitivity_all'`
     update
 done
