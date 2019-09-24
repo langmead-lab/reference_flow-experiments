2d3b6eebfda1e56c71d63cc0a167756cbe14330b
milkschen <s93rm6@gmail.com>
2019-09-23 20:12:10 -0400

    simplified merge() using pandas df processing

--- scripts/refbias/lift_ref_flow.py
+++ scripts/refbias/lift_ref_flow.py
@@ -1,5 +1,6 @@
 from sys import exit
 import argparse
+import pandas as pd
 #from os import path
 
 def main(fn_vcf, list_fn_sam, fn_fasta, list_fn_name, fn_out):
@@ -29,79 +30,28 @@ def main(fn_vcf, list_fn_sam, fn_fasta, list_fn_name, fn_out):
     merge(ref_bi_list, fn_out)
     
 def merge(ref_bi_list, fn_out):
-    #question, how should i handle the HET sites when merging (b/c they are different depending on offsets
-    het_site = []
-    ref_count = []
-    alt_count = []
-    gap_count = []
-    other_count = []
-    num_reads = []
-    chr = 21
-    for i in range(len(ref_bi_list)):
-        count = 0
-        f_in = open(ref_bi_list[i], 'r') 
-        for line in f_in:
-            if '#' in line:
-                continue
-            else:
-                #print("count: ", count)
-                #print("line: ", line)
-                spl = line.split()
-                if i == 0:
-                    if line == '\n':
-                        het_site.append([])
-                        ref_count.append(0)
-                        alt_count.append(0)
-                        gap_count.append(0)
-                        other_count.append(0)
-                        num_reads.append(0)
-                    else:
-                        het_site.append([int(spl[1])])
-                        ref_count.append(int(spl[3]))
-                        alt_count.append(int(spl[4]))
-                        gap_count.append(int(spl[5]))
-                        other_count.append(int(spl[6]))
-                        num_reads.append(int(spl[7]))
-                else: 
-                    if line == '\n':
-                        count += 1
-                        continue
-                    if count-1 < len(het_site): 
-                        het_site[count].append(int(spl[1]))
-                        ref_count[count] = ref_count[count] + int(spl[3])
-                        alt_count[count] = alt_count[count] + int(spl[4])
-                        gap_count[count] = gap_count[count] + int(spl[5])
-                        other_count[count] = other_count[count] + int(spl[6])
-                        num_reads[count] = num_reads[count] + int(spl[7])
+    list_df = []
+    #: read TSVs from sub files
+    for i, bias_file in enumerate(ref_bi_list):
+        sub_df = pd.read_csv(bias_file, sep = '\t', comment = '#')
+        list_df.append(sub_df)
+        if i > 0:
+            #: check columns are matched
+            assert ((sub_df.columns) == (list_df[0].columns)).all()
+            #: check shapes are matched
+            assert sub_df.shape == list_df[0].shape
+            #: check HET sites are matched
+            assert (sub_df[['CHR', 'HET_SITE']] == list_df[0][['CHR', 'HET_SITE']]).all().all()
 
-            count += 1
-
-    f_out = open(fn_out, 'w')
-    f_out.write("#CHR\tHET SITE\tREFERENCE BIAS\tREF COUNT\tALT COUNT\tGAP COUNT\tOTHER COUNT\t# READS")
-    f_out.write("\n")
-    for i in range(len(ref_count)): 
-        f_out.write(str(chr))
-        f_out.write("\t")
-        f_out.write(str(het_site[i]))
-        #f_out.write(str(het_site[i] + 1)) #: 1-based
-        f_out.write("\t")
-        add = ref_count[i] + alt_count[i] + gap_count[i] + other_count[i]
-        if add == 0:
-            f_out.write("n/a")
-        else:
-            ref_bi = float(ref_count[i])/float(ref_count[i] + alt_count[i] + gap_count[i] + other_count[i])
-            f_out.write(str(ref_bi))
-        f_out.write("\t")
-        f_out.write(str(ref_count[i]))
-        f_out.write("\t")
-        f_out.write(str(alt_count[i]))
-        f_out.write("\t")
-        f_out.write(str(gap_count[i]))
-        f_out.write("\t")
-        f_out.write(str(other_count[i]))
-        f_out.write("\t")
-        f_out.write(str(num_reads[i]))
-        f_out.write("\n")
+    df_out = pd.DataFrame(columns = list_df[0].columns)
+    df_out[['CHR', 'HET_SITE']] = list_df[0][['CHR', 'HET_SITE']]
+    df_out[['REF_COUNT', 'ALT_COUNT', 'GAP_COUNT', 'OTHER_COUNT', 'NUM_READS']] = list_df[0][['REF_COUNT', 'ALT_COUNT', 'GAP_COUNT', 'OTHER_COUNT', 'NUM_READS']]
+    for i, sub_df in enumerate(list_df):
+        if i > 0:
+            df_out[['REF_COUNT', 'ALT_COUNT', 'GAP_COUNT', 'OTHER_COUNT', 'NUM_READS']] += sub_df[['REF_COUNT', 'ALT_COUNT', 'GAP_COUNT', 'OTHER_COUNT', 'NUM_READS']]
+    df_out['REFERENCE_BIAS'] = df_out['REF_COUNT'] / df_out['NUM_READS']
+    df_out.to_csv(fn_out, sep = '\t', index = None, float_format = '%.4f')
+    return 
 
 def get_paths_from_list(fn_list):
     l = []
