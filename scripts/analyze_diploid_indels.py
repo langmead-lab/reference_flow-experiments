d460dd19afc1caecf5c71fbe16475029de67d782
milkschen <s93rm6@gmail.com>
2019-11-18 14:12:47 -0500

    now treats chrom names 21 and chr21 equal

--- scripts/analyze_diploid_indels.py
+++ scripts/analyze_diploid_indels.py
@@ -456,11 +456,13 @@ def count_overlapping_vars(
     '''
     num_var = 0   
     for i in range(g_info.pos, g_info.pos + constants.READ_LEN):
-        if g_info.chrm == constants.MAIN_CHROM or g_info.chrm == constants.CHROM:
+        if g_info.chrm == constants.MAIN_CHROM or g_info.chrm == constants.CHROM or g_info.chrm == 'chr' + constants.MAIN_CHROM or g_info.chrm == 'chr' + constants.CHROM:
+        # if g_info.chrm == constants.MAIN_CHROM or g_info.chrm == constants.CHROM:
         # if g_info.chrm == constants.MAIN_CHROM:
             if main_index.get(i) != None:
                 num_var += 1
-        elif g_info.chrm == constants.ALT_CHROM:
+        elif g_info.chrm == constants.ALT_CHROM or g_info.chrm == 'chr' + constants.ALT_CHROM:
+        # elif g_info.chrm == constants.ALT_CHROM:
             if alt_index.get(i) != None:
                 num_var += 1
         else:
